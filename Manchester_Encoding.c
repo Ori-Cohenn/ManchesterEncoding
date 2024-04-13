d7455a6e080242d712ac06c4303573aabd384f8b
Ori Cohen <oricoh100@gmail.com>
2024-04-13 19:48:48 +0300

    Working read and write

--- Manchester_Encoding.c
+++ Manchester_Encoding.c
@@ -1,44 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "Manchester_Encoding.h"
 
-// Function to perform Manchester encoding
-void manchesterEncode(const unsigned char *data, int dataSize, char *encodedData)
+// Encoding
+char manchesterEncode(const char *data, int dataSize, char *encodedData)
 {
     int encodedIndex = 0;
     for (int i = 0; i < dataSize; i++)
     {
-        if ((data[i] & 1) == 0)
+        if ((data[i] & 1) == 0) // High to low transition for 0
         {
-            encodedData[encodedIndex++] = '0'; // High to low transition for '0'
+            encodedData[encodedIndex++] = '0';
             encodedData[encodedIndex++] = '1';
+        }
+        else if ((data[i] & 1) == 1)
+        // Low to high transition for 1
+        {
+            encodedData[encodedIndex++] = '1';
+            encodedData[encodedIndex++] = '0';
         }
         else
         {
-            encodedData[encodedIndex++] = '1'; // Low to high transition for '1'
-            encodedData[encodedIndex++] = '0';
+            printf("Error: Invalid data.\n");
+            return 1;
         }
     }
+    return 0;
 }
 
-// Function to perform Manchester decoding
-void manchesterDecode(const char *encodedData, int dataSize, char *decodedData)
+// Decoding
+char manchesterDecode(char *encodedData, int dataSize, char *decodedData)
 {
     int decodedIndex = 0;
     for (int i = 0; i < dataSize; i += 2)
     {
-        if (encodedData[i] == '0' && encodedData[i + 1] == '1')
+        if (encodedData[i] == '0' && encodedData[i + 1] == '1') // Bit 0 detected
         {
-            decodedData[decodedIndex++] = 0; // Bit '0' detected
+            decodedData[decodedIndex++] = '0';
         }
-        else if (encodedData[i] == '1' && encodedData[i + 1] == '0')
+        else if (encodedData[i] == '1' && encodedData[i + 1] == '0') // Bit 1 detected
         {
-            decodedData[decodedIndex++] = 1; // Bit '1' detected
+            decodedData[decodedIndex++] = '1';
         }
         else
         {
             printf("Error: Invalid Manchester encoding sequence.\n");
-            return;
+            return 1;
         }
     }
+    return 0;
 }
--- adafruit_feather_UART.ino
+++ adafruit_feather_UART.ino
@@ -2,47 +2,59 @@
 #include <Arduino.h>
 #include <stdio.h>
 #include <stdlib.h>
-#define size 100
-char stringReceived[size];
+
+#define BufferSize 10 // 10 bytes buffer
 int stringIndex = 0;
-const unsigned char originalBits[] = {1, 1, 1, 1, 0};
+const char originalBits[] = {1, 1};
 int dataSize = 0;
 char *stringSend;
+char *stringReceived;
 void setup()
 {
   Serial.begin(9600);  // Initialize the Serial monitor for debugging
   Serial1.begin(9600); // Initialize Serial1 for sending data
   dataSize = sizeof(originalBits) / sizeof(originalBits[0]);
   stringSend = (char *)malloc(dataSize * 2);
+  stringReceived = (char *)malloc(BufferSize);
 }
 
 void readUart(void)
 {
-  while ((Serial1.available()) || stringReceived[stringIndex] == 13)
+  while ((Serial1.available()) || stringSend[stringIndex] == 13) // 13 is the ASCII value of carriage return
   {
-    stringReceived[stringIndex] = Serial1.read();
+    stringSend[stringIndex] = Serial1.read();
     stringIndex++;
   }
-  stringIndex = 0;
+  Serial.print("\nManchester Encoded Data Received: ");
+  Serial.println(stringSend);
+  manchesterDecode(stringSend, BufferSize, stringReceived);
+  Serial.print("\nManchester Decoded Data: ");
   Serial.println(stringReceived);
-  memset(stringReceived, 0, size); // clear buffer
+  stringIndex = 0;
+  memset(stringSend, 0, BufferSize);     // clear buffer
+  memset(stringReceived, 0, BufferSize); // clear buffer
 }
 
-void loop()
+void writeUart(void)
 {
-  // readUart();
-  Serial.println("\nOriginal Bits: ");
-  for (int i = 0; i < dataSize; i++)
-  {
-    Serial.print(originalBits[i]);
-  }
   manchesterEncode(originalBits, dataSize, stringSend);
   Serial.println("\nManchester Encoded Data: ");
   for (int i = 0; i < dataSize * 2; i++)
   {
-    Serial.print(stringSend[i]);
+    Serial.print(stringSend[i], DEC);
   }
-  Serial1.write(stringSend);
+  Serial1.write(stringSend, dataSize * 2);
   memset(stringSend, 0, dataSize * 2); // clear buffer
+}
+
+void loop()
+{
+  Serial.println("\nOriginal Bits: ");
+  for (int i = 0; i < dataSize; i++)
+  {
+    Serial.print(originalBits[i], DEC);
+  }
+  writeUart();
+  //readUart();
   delay(1000);
 }
