b5e8f7e0e7518d11fe86f71b14c0c222970fbe57
Ori Cohen <oricoh100@gmail.com>
2024-04-13 11:59:42 +0300

    Format code

--- adafruit_feather_UART.ino
+++ adafruit_feather_UART.ino
@@ -4,39 +4,45 @@
 #include <stdlib.h>
 #define size 100
 char stringReceived[size];
-int stringIndex=0;
-const unsigned char originalBits[] = {1,1,1,1,0};
-int dataSize=0;
-char* stringSend;
-void setup() {
-  Serial.begin(9600);    // Initialize the Serial monitor for debugging
-  Serial1.begin(9600);   // Initialize Serial1 for sending data
+int stringIndex = 0;
+const unsigned char originalBits[] = {1, 1, 1, 1, 0};
+int dataSize = 0;
+char *stringSend;
+void setup()
+{
+  Serial.begin(9600);  // Initialize the Serial monitor for debugging
+  Serial1.begin(9600); // Initialize Serial1 for sending data
   dataSize = sizeof(originalBits) / sizeof(originalBits[0]);
-  stringSend= (char*)malloc(dataSize * 2);
+  stringSend = (char *)malloc(dataSize * 2);
 }
 
-void readUart(void){
-  while((Serial1.available())||stringReceived[stringIndex]==13){
-    stringReceived[stringIndex]=Serial1.read();
+void readUart(void)
+{
+  while ((Serial1.available()) || stringReceived[stringIndex] == 13)
+  {
+    stringReceived[stringIndex] = Serial1.read();
     stringIndex++;
   }
-  stringIndex=0;
+  stringIndex = 0;
   Serial.println(stringReceived);
-  memset(stringReceived,0,size); // clear buffer
+  memset(stringReceived, 0, size); // clear buffer
 }
 
-void loop() {
-  //readUart();
+void loop()
+{
+  // readUart();
   Serial.println("\nOriginal Bits: ");
-  for (int i = 0; i < dataSize; i++) {
-      Serial.print(originalBits[i]);
+  for (int i = 0; i < dataSize; i++)
+  {
+    Serial.print(originalBits[i]);
   }
   manchesterEncode(originalBits, dataSize, stringSend);
   Serial.println("\nManchester Encoded Data: ");
-    for(int i = 0; i < dataSize * 2; i++) {
-        Serial.print(stringSend[i]);
+  for (int i = 0; i < dataSize * 2; i++)
+  {
+    Serial.print(stringSend[i]);
   }
   Serial1.write(stringSend);
-  memset(stringSend,0,dataSize * 2); // clear buffer
+  memset(stringSend, 0, dataSize * 2); // clear buffer
   delay(1000);
 }
