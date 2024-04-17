47a319112fd2a0d9519ceb4894625ca9d580e56b
Ori Cohen <oricoh100@gmail.com>
2024-04-17 21:26:06 +0300

    adding interupt for ditecting falling edge for start condition and many more...

--- adafruit_feather_GPIO.ino
+++ adafruit_feather_GPIO.ino
@@ -5,13 +5,13 @@
 
 #define WRITE_PIN A2
 #define READ_PIN A3
-#define DELAY_TIME 104
+//#define DELAY_TIME 104
+#define DELAY_TIME 500
 #define MAX_DATA_SIZE 40 // 10 bytes
 #define ENCODED_MAX_DATA_SIZE 80 // 10 bytes *2 (manchester encoding)
 #define INTIAL_STATE HIGH
 int mode = 1; // 0 for write, 1 for read
 // 1/9600= 104.16 microseconds
-
 
 void writeSetup(){
     pinMode(WRITE_PIN, OUTPUT);
@@ -20,51 +20,83 @@ void writeSetup(){
 
 void writeLoop()
 {
+    Serial.println("idle");
     digitalWrite(WRITE_PIN, HIGH);
-    delayMicroseconds(DELAY_TIME);
+    //delayMicroseconds(DELAY_TIME);
+    delay(2000);
+    Serial.println("start");
     digitalWrite(WRITE_PIN, LOW);
+    // delayMicroseconds(DELAY_TIME);
+    delay(500);
+    Serial.println("data");
+    digitalWrite(WRITE_PIN, HIGH);
+    // delayMicroseconds(DELAY_TIME);
+    delay(500);
+    digitalWrite(WRITE_PIN, LOW);
+    // delayMicroseconds(DELAY_TIME);
+    delay(1000);
+    digitalWrite(WRITE_PIN, HIGH);
+    // delayMicroseconds(DELAY_TIME);
+    delay(500);
+    digitalWrite(WRITE_PIN, LOW);
+    // delayMicroseconds(DELAY_TIME);
+    delay(1000);
+    digitalWrite(WRITE_PIN, HIGH);
+    // delayMicroseconds(DELAY_TIME);
+    delay(500);
+    //manchester encoding of 1010
 }
 
-// 1/9600= 104.16 microseconds
 void readSetup()
 {
-    pinMode(READ_PIN, INPUT);
+    pinMode(READ_PIN, INPUT_PULLDOWN);
+    attachInterrupt(digitalPinToInterrupt(READ_PIN), readStart, FALLING);
 }
 
-void readLoop()
+void readStart() // detect start condition
+{
+    detachInterrupt(digitalPinToInterrupt(READ_PIN));
+    readData();
+    attachInterrupt(digitalPinToInterrupt(READ_PIN), readStart, FALLING);
+}
+
+void readData()
 {
     int currentState = digitalRead(READ_PIN); // intial state
-    char *receivedData = (char *)malloc(ENCODED_MAX_DATA_SIZE);
-    char *decodedData = (char *)malloc(ENCODED_MAX_DATA_SIZE/2);
-    memset(decodedData,0,ENCODED_MAX_DATA_SIZE/2);
+    char *receivedData = (char *)calloc(ENCODED_MAX_DATA_SIZE, sizeof(char));
+    char *decodedData = (char *)calloc(ENCODED_MAX_DATA_SIZE/2, sizeof(char));
+    //memset(decodedData,0,ENCODED_MAX_DATA_SIZE/2);
     int index = 0;
     int lastState = currentState;
+    Serial.println("ISR");
     Serial.println(currentState);
-    while (currentState == INTIAL_STATE) // idle state waiting for start condition
-    {
-        currentState = digitalRead(READ_PIN);
-        Serial.println(currentState);
-    }
+    // while (currentState == INTIAL_STATE) // while idle
+    // {
+    //     currentState = digitalRead(READ_PIN);
+    // }
     
-    // Read Manchester encoded data
-    
+    // // Read Manchester encoded data
+
     unsigned long lastChangeTime = micros();
-    while (index < ENCODED_MAX_DATA_SIZE && ((micros() - lastChangeTime < 4 * DELAY_TIME)))
+    Serial.println(lastChangeTime);
+    while ((index < ENCODED_MAX_DATA_SIZE) && ((micros() - (lastChangeTime < 4 * DELAY_TIME))))
     {
         currentState = digitalRead(READ_PIN);
-        Serial.println(currentState);
         if (currentState != lastState)
-        {
+        {  
             receivedData[index++] = currentState == HIGH ? '1' : '0';
             lastChangeTime = micros();
+            Serial.println(lastChangeTime);
             lastState = currentState;
         }
     }
+    Serial.println("end"); 
     int isDecoded=manchesterDecode(receivedData, index, decodedData);
+    
     if(isDecoded==0)
     {
-        Serial.println("Decoded data:");
-        Serial.println(decodedData);
+        // Serial.println("Decoded data:");
+        // Serial.println(decodedData);
     }
     else
     {
@@ -95,7 +127,7 @@ void loop()
   }
   if (mode == 1)
   {
-    readLoop();
+  //inerupt will handle the read
+    
   }
-  delay(1000);
 }
