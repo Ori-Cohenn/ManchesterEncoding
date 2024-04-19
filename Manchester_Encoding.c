1ecc9031fedbb056744f0ef78f2f892fa95a6af6
Ori Cohen <oricoh100@gmail.com>
2024-04-19 13:40:02 +0300

    check point to retrive if needed

--- Manchester_Encoding.c
+++ Manchester_Encoding.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 // Encoding
-int manchesterEncode(const char *data, int dataSize, char *encodedData)
+int manchesterEncode(const uint8_t *data, int dataSize, uint8_t *encodedData)
 {
     int encodedIndex = 0;
     for (int i = 0; i < dataSize; i++)
     {
         if ((data[i] & 1) == 0) // High to low transition for 0
         {
-            encodedData[encodedIndex++] = '0';
-            encodedData[encodedIndex++] = '1';
+            encodedData[encodedIndex++] = 0;
+            encodedData[encodedIndex++] = 1;
         }
         else if ((data[i] & 1) == 1) // Low to high transition for 1
         {
-            encodedData[encodedIndex++] = '1';
-            encodedData[encodedIndex++] = '0';
+            encodedData[encodedIndex++] = 1;
+            encodedData[encodedIndex++] = 0;
         }
         else
         {
@@ -27,18 +28,18 @@ int manchesterEncode(const char *data, int dataSize, char *encodedData)
 }
 
 // Decoding
-int manchesterDecode(char *encodedData, int dataSize, char *decodedData)
+int manchesterDecode(uint8_t *encodedData, int dataSize, uint8_t *decodedData)
 {
     int decodedIndex = 0;
     for (int i = 0; i < dataSize; i += 2)
     {
-        if (encodedData[i] == '0' && encodedData[i + 1] == '1') // Bit 0 detected
+        if (encodedData[i] == 0 && encodedData[i + 1] == 1) // Bit 0 detected
         {
-            decodedData[decodedIndex++] = '0';
+            decodedData[decodedIndex++] = 0;
         }
-        else if (encodedData[i] == '1' && encodedData[i + 1] == '0') // Bit 1 detected
+        else if (encodedData[i] == 1 && encodedData[i + 1] == 0) // Bit 1 detected
         {
-            decodedData[decodedIndex++] = '1';
+            decodedData[decodedIndex++] = 1;
         }
         else
         {
--- adafruit_feather_GPIO.ino
+++ adafruit_feather_GPIO.ino
@@ -6,111 +6,18 @@
 #define WRITE_PIN A2
 #define READ_PIN A3
 //#define DELAY_TIME 104
-#define DELAY_TIME 500000
-#define MAX_DATA_SIZE 40 // 10 bytes
-#define ENCODED_MAX_DATA_SIZE 80 // 10 bytes *2 (manchester encoding)
+#define DELAY_TIME 5000000
+#define MAX_DATA_BYTES 10 // 10 bytes
+#define ENCODED_MAX_DATA_BYTES 80 // 10 bytes *2 (manchester encoding)
 #define INTIAL_STATE HIGH
-int mode = 1; // 0 for write, 1 for read
+int mode = 0; // 0 for write, 1 for read
 // 1/9600= 104.16 microseconds
 
-void writeSetup(){
-    pinMode(WRITE_PIN, OUTPUT);
-    digitalWrite(WRITE_PIN, INTIAL_STATE); //intial state is idle
-}
-
-void writeLoop()
-{
-    Serial.println("idle");
-    digitalWrite(WRITE_PIN, HIGH);
-    //delayMicroseconds(DELAY_TIME);
-    delay(2000); // 1111
-    Serial.println("start");
-    digitalWrite(WRITE_PIN, LOW);
-    // delayMicroseconds(DELAY_TIME);
-    delay(500); // 0
-    Serial.println("data");
-    digitalWrite(WRITE_PIN, HIGH);
-    // delayMicroseconds(DELAY_TIME);
-    delay(500); // 1
-    digitalWrite(WRITE_PIN, LOW);
-    // delayMicroseconds(DELAY_TIME);
-    delay(1000); // 00
-    digitalWrite(WRITE_PIN, HIGH);
-    // delayMicroseconds(DELAY_TIME);
-    delay(500); // 1
-    digitalWrite(WRITE_PIN, LOW);
-    // delayMicroseconds(DELAY_TIME);
-    delay(500); //0
-    digitalWrite(WRITE_PIN, HIGH);
-    // delayMicroseconds(DELAY_TIME);
-    delay(500); // 1
-    Serial.println("stop");
-    digitalWrite(WRITE_PIN, HIGH);
-    //delayMicroseconds(DELAY_TIME);
-    delay(2000); // 1111
-    //packet = 1111010010111111-> 100
-}
-
-void readSetup()
-{
-    pinMode(READ_PIN, INPUT);
-    attachInterrupt(digitalPinToInterrupt(READ_PIN), readStart, FALLING);
-}
-
-void readStart() // detect start condition
-{
-    detachInterrupt(digitalPinToInterrupt(READ_PIN));
-    delayMicroseconds(100); 
-    readData();
-    attachInterrupt(digitalPinToInterrupt(READ_PIN), readStart, FALLING);
-    delayMicroseconds(100);
-}
-
-void readData()
-{
-    int currentState = digitalRead(READ_PIN); // intial state
-    char *receivedData = (char *)calloc(ENCODED_MAX_DATA_SIZE, sizeof(char));
-    char *decodedData = (char *)calloc(ENCODED_MAX_DATA_SIZE/2, sizeof(char));
-    //memset(decodedData,0,ENCODED_MAX_DATA_SIZE/2);
-    int index = 0;
-    int lastState = currentState;
-    Serial.println("ISR");
-    Serial.println(currentState);
-    // while (currentState == INTIAL_STATE) // while idle
-    // {
-    //     currentState = digitalRead(READ_PIN);
-    // }
-    
-    // // Read Manchester encoded data
-    unsigned long lastChangeTime = micros(); 
-    while ((index < ENCODED_MAX_DATA_SIZE) && ((micros() - lastChangeTime) < 4 * DELAY_TIME))
-    {
-        currentState = digitalRead(READ_PIN);
-        if (currentState != lastState)
-        {  
-            receivedData[index++] = currentState == HIGH ? '1' : '0';
-            Serial.println("Received data:");
-            Serial.println(receivedData[index-1]);
-            lastChangeTime = micros();
-            Serial.println(lastChangeTime);
-            lastState = currentState;
-        }
-    }
-    Serial.println(receivedData); 
-    int isDecoded=manchesterDecode(receivedData, index, decodedData);
-    
-    if(isDecoded==0)
-    {
-        Serial.println("Decoded data:");
-        Serial.println(decodedData);
-    }
-    else
-    {
-        Serial.println("Error: Invalid Manchester encoding sequence.");
-    }
-    free(receivedData);
-    free(decodedData);
-}
+void writeSetup();
+void writeLoop();
+void readSetup();
+void readStart();
+void readData();
 
 void setup()
 {
@@ -119,7 +26,7 @@ void setup()
   {
     writeSetup();
   }
-  if (mode == 1)
+  else if (mode == 1)
   {
     readSetup();
   }
@@ -131,9 +38,125 @@ void loop()
   {
     writeLoop();
   }
-  if (mode == 1)
+  else if (mode == 1)
   {
   //inerupt will handle the read
-    
   }
 }
+
+void writeSetup(){
+    pinMode(WRITE_PIN, OUTPUT);
+    digitalWrite(WRITE_PIN, INTIAL_STATE); //intial state is idle
+}
+
+void writeLoop()
+{
+  uint8_t data[MAX_DATA_BYTES] = {0x03,0x01,0x00};// 10 bytes
+  int dataSize = sizeof(data) / sizeof(data[0]);
+  uint8_t datainbits[dataSize*8];
+  Serial.print("\nData:");
+  for(int i=0;i<dataSize;i++)
+  {
+    for(int j=0;j<8;j++)
+    {
+      datainbits[i*8+j]=(data[i]>>j)&1;
+      Serial.print(datainbits[i*8+j]);
+    }
+  }
+  int encodedDataSize=dataSize*8*2;
+  uint8_t encodedData[encodedDataSize];
+  int isEncoded=manchesterEncode(datainbits,encodedDataSize,encodedData);
+  Serial.print("\nEncoded data:");
+  for(int i=0;i<encodedDataSize;i++)
+  {
+    Serial.print(encodedData[i]);
+  }
+  if(isEncoded==0)
+  {
+    // for(int i=0;i<ENCODED_MAX_DATA_BYTES;i++)
+    // {
+    //   digitalWrite(WRITE_PIN,encodedData[i]);
+    //   delayMicroseconds(DELAY_TIME);
+    // }
+  }
+  else
+  {
+    Serial.println("Error: Invalid data.");
+  }
+}
+
+void readSetup()
+{
+    pinMode(READ_PIN, INPUT);
+    attachInterrupt(digitalPinToInterrupt(READ_PIN), readStart, FALLING);
+}
+
+void readStart() // detect start condition
+{
+    detachInterrupt(digitalPinToInterrupt(READ_PIN)); 
+    // wait for start condition to end 
+    while(digitalRead(READ_PIN)==LOW);
+    readData();
+    attachInterrupt(digitalPinToInterrupt(READ_PIN), readStart, FALLING);
+}
+
+void readData()
+{
+    int currentState = digitalRead(READ_PIN); // intial state
+    uint8_t *receivedData = (uint8_t *)calloc(ENCODED_MAX_DATA_BYTES, sizeof(uint8_t));
+    uint8_t *decodedData = (uint8_t *)calloc(ENCODED_MAX_DATA_BYTES/2, sizeof(uint8_t));
+    //memset(decodedData,0,ENCODED_MAX_DATA_BYTES/2);
+    int index = 0;
+    int lastState = currentState;
+    Serial.println("ISR");
+    Serial.println(currentState);
+    // while (currentState == INTIAL_STATE) // while idle
+    // {
+    //     currentState = digitalRead(READ_PIN);
+    // }
+    
+    // // Read Manchester encoded data
+    unsigned long lastChangeTime = micros(); 
+    while (1)
+    {
+        currentState = digitalRead(READ_PIN);
+        if (currentState != lastState)
+        {
+            lastChangeTime = micros();
+            lastState = currentState;
+            if (currentState == HIGH)
+            {
+                receivedData[index++] = 1;
+            }
+            else
+            {
+                receivedData[index++] = 0;
+            }
+        }
+        // stop condition is when the data is 1111 
+        if ((receivedData[index - 4] == 1 && receivedData[index - 3] == 1 && receivedData[index - 2] == 1 && receivedData[index - 1] == 1) || index >= ENCODED_MAX_DATA_BYTES)
+        {
+          // remove the stop condition
+            break;
+        }
+    }
+    Serial.println("Received data:");
+
+    int isDecoded=manchesterDecode(receivedData, index, decodedData);
+    
+    if(isDecoded==0)
+    {
+        Serial.println("Decoded data:");
+        for (int i = 0; i < index / 2; i++)
+        {
+            Serial.print(decodedData[i]);
+        }
+    }
+    else
+    {
+        Serial.println("Error: Invalid Manchester encoding sequence.");
+    }
+    free(receivedData);
+    free(decodedData);
+}
+
